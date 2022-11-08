7af960a6a4a64b8eb5d965244792304969f74d93
Gabriela Dyworová <gabriela.dyworova@gmail.com>
2022-11-08 13:04:05 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string konfig = "Fullscreen shaDows autosave";
+konfig = konfig.ToLower();
+Console.WriteLine("Poběží hra ve fullscreenu?");
+Console.WriteLine(konfig.Contains("fullscreen"));
+Console.WriteLine("Budou zapnuté stíny?");
+Console.WriteLine(konfig.Contains("shadows"));
+Console.WriteLine("Přeje si hráč vypnout zvuk?");
+Console.WriteLine(konfig.Contains("nosound"));
+Console.WriteLine("Přeje si hráč hru automaticky ukládat?");
+Console.WriteLine(konfig.Contains("autosave"));
+Console.ReadKey();
